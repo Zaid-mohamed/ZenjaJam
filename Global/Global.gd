e41281c2ca644105b5dd8d133d1924bfc7985fd1
Zaid-mohamed <zaidmohamedhmd@gmail.com>
2024-03-02 23:01:09 +0200

    سس

--- Global/Global.gd
+++ Global/Global.gd
@@ -1,19 +1,18 @@
-extends Node
+extends AnimationPlayer
 
 class_name global
 
-var anim
+
 
 
 
 func _ready():
-	anim = get_node("/root/Global/AnimationPlayer")
 	add_to_group("global")
+	add_child(self)
 func Open():
-	anim.play("OpenUp")
-	await anim.animation_finished
+	play("OpenUp")
+
 func Close():
 
-	anim.play("Close")
-	await anim.animation_finished
+	play("Close")
 	
--- menu.gd
+++ menu.gd
@@ -5,13 +5,16 @@ extends Control
 
 func _on_play_pressed():
 	global.Close()
+	await get_tree().create_timer(50)
 	get_tree().change_scene_to_file("res://MainScene/main.tscn")
 
 
 func _on_credits_pressed():
 	global.Close()
+	await get_tree().create_timer(0.5)
+	
 	get_tree().change_scene_to_file("res://Credits.tscn")
 
 
 func _on_exit_pressed():
-	pass # Replace with function body.
+	get_tree().quit()
