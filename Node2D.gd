cfd8483facfe74f86e03f493bac5cf8df462f967
serdevai <156949818+serdevai@users.noreply.github.com>
2024-03-02 12:16:49 +0100

    red

---
+++ Node2D.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _physics_process(delta):
+	look_at(get_global_mouse_position())
+	
+	
+	
+	
+	
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
