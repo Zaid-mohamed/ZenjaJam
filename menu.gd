ccb8b732d1d0fca04527d3b890c2d6cacc2f4ff8
serdevai <156949818+serdevai@users.noreply.github.com>
2024-03-03 18:22:03 +0100

    aaa

--- menu.gd
+++ menu.gd
@@ -1,18 +1,22 @@
 extends Control
 
-
+@onready var Global = get_node("/root/Global")
+@onready var timer = $Timer
 
 
 
 func _on_play_pressed():
 	
+	Global.change_scene("res://MainScene/main.tscn")
+	timer.start()
+	await timer.timeout
 	get_tree().change_scene_to_file("res://MainScene/main.tscn")
 
 
 func _on_credits_pressed():
 	
 	
-	
+	await get_tree().create_timer(0.5).timeout
 	get_tree().change_scene_to_file("res://control.tscn")
 
 
