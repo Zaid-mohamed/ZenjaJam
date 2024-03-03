76f0d8f853ad71d7a5545eff9421de85fe54554e
Zaid-mohamed <zaidmohamedhmd@gmail.com>
2024-03-03 22:06:41 +0200

    Update crysal.gd

--- Crystal/crysal.gd
+++ Crystal/crysal.gd
@@ -2,7 +2,7 @@ extends StaticBody2D
 
 class_name crystal
 var hope_level : int = 100 : set = set_hope_level
-@onready var temp_hope_label : Label = get_node("UI/Label")
+@onready var temp_hope_label : Label = get_node("UI/HopeLabel")
 
 # setters
 
