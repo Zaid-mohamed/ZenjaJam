05dcb43ac50bfecf0bc909512b946f9c0e3f72e5
Almajoule <ouffataishak34@gmail.com>
2024-03-03 12:11:39 +0100

    ad timer

--- menu.gd
+++ menu.gd
@@ -1,12 +1,15 @@
 extends Control
 
 @onready var Global  = get_node("/root/Global")
-
+@onready var timer = $Timer
 
 
 func _on_play_pressed():
-	
 	Global.change_scene("res://MainScene/main.tscn")
+	timer.start()
+	await timer.timeout
+	get_tree().change_scene_to_file("res://MainScene/main.tscn")
+	
 
 
 func _on_credits_pressed():
